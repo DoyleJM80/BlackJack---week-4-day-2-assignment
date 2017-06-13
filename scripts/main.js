f6eaf4d5cb3969076aeea49f6cc6ecbea946bd64
Jamie Doyle <thedoyles922@users.noreply.github.com>
2017-06-13 15:32:06 -0400

    finished project

--- scripts/main.js
+++ scripts/main.js
@@ -5,17 +5,32 @@
    to watch the assertions pass as you write your code.
 
    Also remember, that the parameter `hand` will be an array, so
-   you'll need to parse through that first before you can start to 
+   you'll need to parse through that first before you can start to
    write your logic.
 */
 
 
 function handValue (hand) {
+  var cards = hand;
+  var total = 0;
+  for (var i = 0; i < cards.length; i++) {
+    if (cards[i] === 'J' || cards[i] === 'Q' || cards[i] === 'K') {
+      cards[i] = 10;
+    }
+    if (cards[i] === 'A' && total === 8) {
+      cards[i] = 1;
+    } else if (cards[i] === 'A' && total <= 9) {
+        cards[i] = 11;
+    } else if (cards[i] === 'A' && total >= 10){
+      cards[i] = 1;
+    }
+    cards[i] = +cards[i];
+    total += cards[i];
+  }
+  console.log(total);
 
-
-  return;
+  return total;
 }
-
 
 /* -----  Hints ------
 
